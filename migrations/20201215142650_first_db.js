6bb68b782bce92e548b073fa5d760688e6593687
LaShawn <lts001@gmail.com>
2020-12-15 14:32:11 +0900

    added migrations

---
+++ migrations/20201215142650_first_db.js
@@ -0,0 +1,16 @@
+exports.up = function (knex) {
+  return knex.schema.createTable("doctors", (table) => {
+    table.increments();
+    table.text("stripe_id");
+    table.text("first_name");
+    table.text("last_name");
+    table.text("specialty");
+    table.text("availability");
+    table.text("image");
+    table.text("zoom");
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable("doctors");
+};

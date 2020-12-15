ae675898615ee3b8969494761d636edd56a35f53
LaShawn <lts001@gmail.com>
2020-12-15 17:00:20 +0900

    updated env info for staging

--- knexfile.js
+++ knexfile.js
@@ -7,36 +7,14 @@ module.exports = {
     connection: 'postgres://localhost/yoursolution'
   },
 
-  // staging: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // },
+  staging: {
+    client: 'pg',
+    connection: 'process.env.DATABASE_URL',
+  },
 
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // }
+  production: {
+    client: 'pg',
+    connection: 'process.env.DATABASE_URL',
+  }
 
 };

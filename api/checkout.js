7ab070d6043530e97dc6c3b3f824ddabfdbae94f
bayezid1989 <bayezid1989@live.jp>
2020-12-15 20:34:06 +0900

    changed filename

---
+++ api/checkout.js
@@ -0,0 +1,66 @@
+var express = require("express");
+var router = express.Router();
+// const { resolve } = require("path");
+require("dotenv").config();
+
+checkEnv();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+router.get("/config", async (req, res) => {
+  const price = await stripe.prices.retrieve(process.env.PRICE);
+
+  res.send({
+    publicKey: process.env.STRIPE_PUBLISHABLE_KEY,
+    unitAmount: price.unit_amount,
+    currency: price.currency,
+  });
+});
+
+// Fetch the Checkout Session to display the JSON result on the success page
+router.get("/checkout-session", async (req, res) => {
+  const { sessionId } = req.query;
+  const session = await stripe.checkout.sessions.retrieve(sessionId);
+  res.send(session);
+});
+
+router.post("/create-checkout-session", async (req, res) => {
+  const domainURL = process.env.DOMAIN;
+
+  const { quantity, locale } = req.body;
+  // Create new Checkout Session for the order
+  // Other optional params include:
+  // [billing_address_collection] - to display billing address details on the page
+  // [customer] - if you have an existing Stripe Customer ID
+  // [customer_email] - lets you prefill the email input in the Checkout page
+  // For full details see https://stripe.com/docs/api/checkout/sessions/create
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: process.env.PAYMENT_METHODS.split(", "),
+    mode: "payment",
+    locale: locale,
+    line_items: [
+      {
+        price: process.env.PRICE,
+        quantity: quantity,
+      },
+    ],
+    // ?session_id={CHECKOUT_SESSION_ID} means the redirect will have the session ID set as a query param
+    success_url: `${domainURL}/success.html?session_id={CHECKOUT_SESSION_ID}`,
+    cancel_url: `${domainURL}/canceled.html`,
+  });
+
+  res.send({
+    sessionId: session.id,
+  });
+});
+
+function checkEnv() {
+  const price = process.env.PRICE;
+  if (price === "price_12345" || !price) {
+    console.log(
+      "You must set a Price ID in the environment variables. Please see the README."
+    );
+    process.exit(0);
+  }
+}
+
+module.exports = router;
--- knexfile.js
+++ knexfile.js
@@ -9,12 +9,10 @@ module.exports = {
   staging: {
     client: "pg",
     connection: process.env.DATABASE_URL,
-    ssl: true,
   },
 
   production: {
     client: "pg",
     connection: process.env.DATABASE_URL,
-    ssl: true,
   },
 };

3317bcd51e6f670608f056dad6e5f940c3271e49
LaShawn <lts001@gmail.com>
2020-12-16 16:53:05 +0900

    added updated endpoint to doctors

--- api/doctors.js
+++ api/doctors.js
@@ -10,4 +10,16 @@ router.get("/", (req, res) => {
   });
 });
 
+router.get("/:id", (req,res) => {
+  queries.getOne(req.params.id).then(doctor => {
+    res.json(doctor)
+  })
+})
+
+router.put("/:id", (req, res, next) => {
+  queries.update(req.params.id, req.body).then(doctors => {
+    res.json(doctors[0]);
+  })
+})
+
 module.exports = router;
--- db/queries.js
+++ db/queries.js
@@ -4,4 +4,10 @@ module.exports = {
   getAll() {
     return knex("doctors");
   },
+  getOne(id) {
+    return knex("doctors").where("id", id).first();
+  },
+  update(id, doctors) {
+    return knex("doctors").where("id", id).update(doctors, "*");
+  },
 };

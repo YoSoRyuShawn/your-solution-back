e03084a89b720e0337cbd774970f606b7cb02273
bayezid1989 <bayezid1989@live.jp>
2021-02-21 23:08:11 +0900

    Added ssl require true

--- knexfile.js
+++ knexfile.js
@@ -9,13 +9,13 @@ module.exports = {
   staging: {
     client: "pg",
     connection: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false },
+    ssl: { require: true, rejectUnauthorized: false },
   },
 
   production: {
     client: "pg",
     connection: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false },
+    ssl: { require: true, rejectUnauthorized: false },
   },
 };
 //please work... --> Yeah it worked after deleting app once in Heroku!
